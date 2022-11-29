2cbe244c0c337965d983f36ccb67669b13fbca81
Jhosep <jose-zromero@hotmail.com>
2022-11-29 12:35:03 -0600

    Install linters and link pages between home and details

---
+++ src/app/pages/detail.js
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+const Details = () => (
+
+  <button type="button">
+    {' '}
+    <NavLink to="/home">regresar a gome</NavLink>
+  </button>
+);
+
+export default Details;
---
+++ src/app/pages/home.js
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+const HomePage = () => (
+
+  <button type="button">
+    {' '}
+    <NavLink to="/details">ir a details </NavLink>
+  </button>
+);
+
+export default HomePage;
